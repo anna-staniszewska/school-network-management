ad1a147a3136ee553a3b1515755b1af87a1a894d
AnnaStaniszewska <97187091+xAniSsx@users.noreply.github.com>
2023-12-18 02:19:18 +0100

    Logowanie i zgłoszenie produktów - całość.

--- demoApp/index.php
+++ demoApp/index.php
@@ -9,22 +9,72 @@ session_start();
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Sieć Szkół</title>
         <link rel="stylesheet" type="text/css" href="./css/index.css">
+        <link rel="shortcut icon" href="./css/logo.jpg">
     </head>
     <body>
-        <p id="zegar"></p>
-        <script>
-            setInterval(myTimer, 1000);
-            function myTimer(){
-                const date = new Date();
-                document.getElementById("zegar").innerHTML = date.toLocaleTimeString();
-            }
-        </script>
+        <header>
+            <div id="logo">
+                <img src="./css/logo.jpg" width=150px>
+            </div>
 
-        <div>
-            <header>
-                <h1>LOGOWANIE</h1>
-            </header>
+            <div id="zegar">
+                <p id="data">
+                    <script language="JavaScript">
+                        DayName = new Array(7)
+                        DayName[0] = "niedziela"
+                        DayName[1] = "poniedziałek"
+                        DayName[2] = "wtorek"
+                        DayName[3] = "środa"
+                        DayName[4] = "czwartek"
+                        DayName[5] = "piątek"
+                        DayName[6] = "sobota"
 
+                        MonthName = new Array(12)
+                        MonthName[0] = "stycznia"
+                        MonthName[1] = "lutego"
+                        MonthName[2] = "marca"
+                        MonthName[3] = "kwietnia"
+                        MonthName[4] = "maja"
+                        MonthName[5] = "czerwca"
+                        MonthName[6] = "lipca"
+                        MonthName[7] = "sierpnia"
+                        MonthName[8] = "września"
+                        MonthName[9] = "października"
+                        MonthName[10] = "listopada"
+                        MonthName[11] = "grudnia"
+
+                        function getDateStr(){
+                            var Today = new Date()
+                            var WeekDay = Today.getDay()
+                            var Month = Today.getMonth()
+                            var Day = Today.getDate()
+                            var Year = Today.getFullYear()
+
+                            if(Year <= 99)
+                                Year += 1900
+
+                            return DayName[WeekDay] + "," + " " + Day + " " + MonthName[Month] + " " + Year
+                        }
+                    </script>
+
+                    <script>document.write("Dzisiaj jest " + getDateStr())</script>
+                </p>
+                
+                <p id="czas"></p>
+                <script>
+                    setInterval(myTimer, 1000);
+
+                    function myTimer(){
+                        const date = new Date();
+                        document.getElementById("czas").innerHTML = date.toLocaleTimeString();
+                    }
+                </script>   
+            </div>     
+        </header>
+
+        <div id="main">
+            <h1>LOGOWANIE</h1>
+        
             <main>
                 <form action="includes/login.php" method="post">
                     <label>Wpisz nazwę użytkownika</label>
--- demoApp/subPages/inputProducts.php
+++ demoApp/subPages/inputProducts.php
@@ -9,35 +9,79 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Zgłoszenia produktów</title>
         <link rel="stylesheet" type="text/css" href="../css/inputProducts.css">
+        <link rel="shortcut icon" href="../css/logo.jpg">
     </head>
 
     <body>
         <header>
-            <div id="logo">
-                <img src="../css/logo.jpg" width=100px>
+            <div class="headerContainer">
+                <div id="logo">
+                    <img src="../css/logo.jpg" width=150px>
+                </div>
+
+                <div id="welcome">
+                    <h1>Cześć, <?php
+                        echo $_SESSION["Imie"];
+                        ?>!
+                    </h1>
+                </div>
+
+                <div id="zegar">
+                    <p id="data">
+                        <script language="JavaScript">
+                            DayName = new Array(7)
+                            DayName[0] = "niedziela"
+                            DayName[1] = "poniedziałek"
+                            DayName[2] = "wtorek"
+                            DayName[3] = "środa"
+                            DayName[4] = "czwartek"
+                            DayName[5] = "piątek"
+                            DayName[6] = "sobota"
+
+                            MonthName = new Array(12)
+                            MonthName[0] = "stycznia"
+                            MonthName[1] = "lutego"
+                            MonthName[2] = "marca"
+                            MonthName[3] = "kwietnia"
+                            MonthName[4] = "maja"
+                            MonthName[5] = "czerwca"
+                            MonthName[6] = "lipca"
+                            MonthName[7] = "sierpnia"
+                            MonthName[8] = "września"
+                            MonthName[9] = "października"
+                            MonthName[10] = "listopada"
+                            MonthName[11] = "grudnia"
+
+                            function getDateStr(){
+                                var Today = new Date()
+                                var WeekDay = Today.getDay()
+                                var Month = Today.getMonth()
+                                var Day = Today.getDate()
+                                var Year = Today.getFullYear()
+
+                                if(Year <= 99)
+                                    Year += 1900
+
+                                return DayName[WeekDay] + "," + " " + Day + " " + MonthName[Month] + " " + Year
+                            }
+                        </script>
+
+                        <script>document.write("Dzisiaj jest " + getDateStr())</script>
+                    </p>
+                
+                    <p id="czas"></p>
+                    <script>
+                        setInterval(myTimer, 1000);
+
+                        function myTimer(){
+                            const date = new Date();
+                            document.getElementById("czas").innerHTML = date.toLocaleTimeString();
+                        }
+                    </script>  
+                </div>
             </div>
 
-            <div id="welcome">
-                <h1>Cześć, <?php
-                    echo $_SESSION["Imie"];
-                    ?>!
-                </h1>
-
-            </div>
-
-            <div id="clockLogOut">
-                <p id="zegar"></p>
-                <a href="../includes/logout.php">Wyloguj</a>
-            </div>
-            <script>
-                setInterval(myTimer, 1000);
-                function myTimer(){
-                    const date = new Date();
-                    document.getElementById("zegar").innerHTML = date.toLocaleTimeString();
-                }
-            </script>
-
-            
+            <a href="../includes/logout.php">Wyloguj</a>
         </header>
 
         <main>
@@ -58,7 +102,6 @@
 
                     <button type = "submit" name = "submit">Zgłoś</button>
             </form>
-
 
             <script>
                 function hide(){
