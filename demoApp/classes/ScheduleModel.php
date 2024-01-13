ce55119d785375f065842e1659f5ef71a30c2ed9
Piotr Powroźnik <97744639+PPowroznik02@users.noreply.github.com>
2024-01-13 10:04:11 +0100

    Rozmowy S2-17: Zmodyfikowano zapytanie sql

--- demoApp/classes/ScheduleModel.php
+++ demoApp/classes/ScheduleModel.php
@@ -1,9 +1,12 @@
 <?php
 session_start();
-class ScheduleModel extends Database {
-  protected function getMeetings($schoolId) {
-      if($_SESSION["Stanowisko"]=="wlasciciel") {
-          $sql = "SELECT Rozmowy.DATA AS Data, DATE_FORMAT(Rozmowy.GodzinaRozpoczecia, '%H:%i') AS GodzinaRozpoczecia,
+
+class ScheduleModel extends Database
+{
+    protected function getMeetings($schoolId)
+    {
+        if ($_SESSION["Stanowisko"] == "wlasciciel") {
+            $sql = "SELECT Rozmowy.IdRozmowy AS IdRozmowy, Rozmowy.DATA AS Data, DATE_FORMAT(Rozmowy.GodzinaRozpoczecia, '%H:%i') AS GodzinaRozpoczecia,
             DATE_FORMAT(Rozmowy.GodzinaZakonczenia, '%H:%i') AS GodzinaZakonczenia,
             Szkola.IdSzkoly, Szkola.Nazwa, Sale.IdSali,
             Kandydaci.Stanowisko, Kandydaci.Imie, Kandydaci.Nazwisko
@@ -12,9 +15,8 @@ class ScheduleModel extends Database {
             JOIN Sale ON Rozmowy.IdSali=Sale.IdSali
             JOIN Szkola ON Sale.IdSzkoly=Szkola.IdSzkoly 
             ORDER BY Rozmowy.GodzinaRozpoczecia";
-      }
-      else {
-          $sql = "SELECT Rozmowy.DATA AS Data, DATE_FORMAT(Rozmowy.GodzinaRozpoczecia, '%H:%i') AS GodzinaRozpoczecia,
+        } else {
+            $sql = "SELECT Rozmowy.IdRozmowy AS IdRozmowy, Rozmowy.DATA AS Data, DATE_FORMAT(Rozmowy.GodzinaRozpoczecia, '%H:%i') AS GodzinaRozpoczecia,
             DATE_FORMAT(Rozmowy.GodzinaZakonczenia, '%H:%i') AS GodzinaZakonczenia,
             Szkola.IdSzkoly, Szkola.Nazwa, Sale.IdSali,
             Kandydaci.Stanowisko, Kandydaci.Imie, Kandydaci.Nazwisko
@@ -24,37 +26,36 @@ class ScheduleModel extends Database {
             JOIN Szkola ON Sale.IdSzkoly=Szkola.IdSzkoly 
             WHERE Szkola.IdSzkoly = ?
             ORDER BY Rozmowy.GodzinaRozpoczecia";
-      }
+        }
 
 
-    $stmt = $this->connect()->prepare($sql);
+        $stmt = $this->connect()->prepare($sql);
 
-      if($_SESSION["Stanowisko"]=="wlasciciel") {
-          if (!$stmt->execute()) {
-              $stmt = null;
-              header("location: ../index.php?error=stmtfailed");
-              exit();
-          }
-      }
-      else {
-          if (!$stmt->execute(array($schoolId))) {
-              $stmt = null;
-              header("location: ../index.php?error=stmtfailed");
-              exit();
-          }
-      }
+        if ($_SESSION["Stanowisko"] == "wlasciciel") {
+            if (!$stmt->execute()) {
+                $stmt = null;
+                header("location: ../index.php?error=stmtfailed");
+                exit();
+            }
+        } else {
+            if (!$stmt->execute(array($schoolId))) {
+                $stmt = null;
+                header("location: ../index.php?error=stmtfailed");
+                exit();
+            }
+        }
 
 
-    if ($stmt->rowCount() == 0) {
-      $stmt = null;
-      //echo "brak zaplanowanych spotkań";
-      header("location: ../index.php?error=meetingsNotFound");
-      exit();
+        if ($stmt->rowCount() == 0) {
+            $stmt = null;
+            //echo "brak zaplanowanych spotkań";
+            header("location: ../index.php?error=meetingsNotFound");
+            exit();
+        }
+
+        $meetings = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $_SESSION["spotkania"] = $meetings;
+
+        $stmt = null;
     }
-
-    $meetings = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    $_SESSION["spotkania"] = $meetings;
-
-    $stmt = null;
-  }
 }
