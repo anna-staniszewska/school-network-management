38022c4ad96c27e7e7c97094e188eb0dc90abdb1
AnnaStaniszewska <97187091+xAniSsx@users.noreply.github.com>
2024-01-15 15:05:43 +0100

    Rozmowy S2-17 i S2-14 - php page

--- demoApp/subPages/subSubPages/modifyMeeting.page.php
+++ demoApp/subPages/subSubPages/modifyMeeting.page.php
@@ -8,8 +8,8 @@ session_start();
     <meta name="viewport" content="width=device-width, inital-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Modyfikacja spotkania</title>
-    <link rel="stylesheet" type="text/css" href="../../css/modifyMeeting.css">
     <link rel="shortcut icon" href="../../css/logo.jpg">
+    <link rel="stylesheet" type="text/css" href="../../css/modifyMeeting.css">
 </head>
 
 <body>
@@ -19,7 +19,7 @@ session_start();
             <img src="../../css/logo.jpg" width=150px>
         </div>
 
-        <h1>Modyfikuj</h1>
+        <h1>Modyfikacja spotkania</h1>
 
         <div id="zegar">
             <p id="data">
@@ -76,17 +76,15 @@ session_start();
         </div>
     </div>
 
-    <a class="button" id="logout" href="../../includes/logout.inc.php">Wyloguj</a>
-    <a class="button" href="../recruitment.page.php">HOME</a>
-
-
+    <a class="button" id="logout" href="../includes/logout.inc.php">Wyloguj</a>
 </header>
 
 
 <main>
-    <div>
+    <div id="meetingInfo">
         <?php
         /*IdRozmowy: <?php echo $_SESSION["IdRozmowy"]; ?>*/
+        echo "<div id='candidate'>";
         $meetingInformations = $_SESSION["spotkanie"][0];
         echo "<h3>Inforamcje o kandydacie</h3>";
         echo "Imie: " . $meetingInformations["Imie"] . "<br>";
@@ -95,45 +93,55 @@ session_start();
         echo "Data urodzenia: " . $meetingInformations["DataUrodzenia"] . "<br>";
         echo "Nr. telefonu: " . $meetingInformations["NrTelefonu"] . "<br>";
         echo "E-mail: " . $meetingInformations["Email"] . "<br>";
-        echo "Miejscowość: " . $meetingInformations["Miejscowosc"] . "<br>";
+        echo "Miejscowosc: " . $meetingInformations["Miejscowosc"] . "<br>";
         echo "Ulica: " . $meetingInformations["Ulica"] . "<br>";
         echo "Nr. budynku: " . $meetingInformations["NrBudynku"] . "<br>";
         if ($meetingInformations["NrLokalu"] != null) {
             echo "Nr. lokalu: " . $meetingInformations["NrLokalu"] . "<br>";
         }
         echo "Pesel: " . $meetingInformations["Pesel"] . "<br>";
+        echo "</div>";
+
+        echo "<div id='meeting'>";
         echo "<h3>Informacje o spotkaniu</h3>";
         echo "Godzina rozpoczęcia: " . $meetingInformations["GodzinaRozpoczecia"] . "<br>";
         echo "Godzina zakończenia: " . $meetingInformations["GodzinaZakonczenia"] . "<br>";
         echo "Data spotkania: " . $meetingInformations["DataRozmowy"] . "<br>";
         echo "Nr. sali: " . $meetingInformations["IdSali"] . "<br>";
+        echo "</div>";
         ?>
     </div>
-
+    <br><br>
     <h2>Zmodyfikuj spotkanie</h2>
+    <br>
     <form action="../../includes/modifyMeeting.inc.php" method="POST">
-        <label>Nr. sali:</label>
-        <br>
-        <input id="text" type="text" name="roomId" placeholder="Nr. sali" minlength="1" maxlength="12"  required>
-        <br>
-        <label>Godzina rozpoczęcia:</label>
-        <br>
-        <input id="time" type="time" name="startTime" placeholder="godzina rozpoczęcia" minlength="5" maxlength="5" required>
-        <br>
-        <label>Godzina zakończenia:</label>
-        <br>
-        <input id="time" type="time" name="endTime" placeholder="godzina zakończenia" minlength="5" maxlength="5" required>
-        <br>
-        <label>Data spotkania</label>
-        <br>
-        <input id="date" type="date" name="meetingDate"  placeholder="dd-mm-yyyy" onclick="hide()" required>
-        <br>
-        <button id ="modify" type="submit" name="submitModify">Modyfikuj</button>
+        <div id="modifyForm">
+            <label>Nr. sali:</label>
+            <br>
+            <input type="text" name="roomId" placeholder="Nr. sali" minlength="1" maxlength="12"  required>
+            <br>
+            <label>Godzina rozpoczęcia:</label>
+            <br>
+            <input type="time" name="startTime" placeholder="godzina rozpoczęcia" minlength="5" maxlength="5" required>
+            <br>
+            <label>Godzina zakończenia:</label>
+            <br>
+            <input type="time" name="endTime" placeholder="godzina zakończenia" minlength="5" maxlength="5" required>
+            <br>
+            <label>Data spotkania</label>
+            <br>
+            <input type="date" name="meetingDate"  placeholder="dd-mm-yyyy" onclick="hide()" required>
+            <br>
+        </div>
+        <button id="modifyButton" type="submit" name="submitModify">Modyfikuj</button>
     </form>
 
     <form action="../../includes/modifyMeeting.inc.php" method="POST">
-        <button id ="delete" type="submit" name="submitDelete">Usuń spotkanie</button>
+        <button type="submit" name="submitDelete">Usuń spotkanie</button>
+        <a href="../recruitment.page.php">Anuluj</a>
     </form>
+
+    
 
 
 </main>
